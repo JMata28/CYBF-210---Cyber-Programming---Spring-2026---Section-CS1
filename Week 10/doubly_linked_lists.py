ee7d3d82dc26a7fec68c148788f47923f8436eb5
Prof Mata <profmata@Joses-MacBook-Pro.local>
2026-04-02 19:36:51 -0400

    Updated content for Week 10 on 4.2.2026

--- Week 10/doubly_linked_lists.py
+++ Week 10/doubly_linked_lists.py
@@ -77,7 +77,7 @@ def delete_specific_node_doubly(head_node, tail_node, node_to_delete):
 
     return head_node, tail_node
 
-head_node, tail_node = delete_specific_node_doubly(head_node, tail_node, tail_node)
+head_node, tail_node = delete_specific_node_doubly(head_node, tail_node, None)
 display_doubly(head_node)
 
 #To insert a new node, assuming what you have is the position (assuming imaginary zero indexing: head_node has an imaginary index of 0, the second node has an imaginary index of 2, and so on and so forth.)
@@ -126,7 +126,7 @@ def insert_Node_At_Position_Doubly(head_node, tail_node, new_node, position):
 D = DoublyNode("hexagon")
 E = DoublyNode("octagon")
 
-head_node, tail_node = insert_Node_At_Position_Doubly(head_node, tail_node, D, 1)
-display_doubly(head_node)
-head_node, tail_node = insert_Node_At_Position_Doubly(head_node, tail_node, E, 2)
-display_doubly(head_node)
+# head_node, tail_node = insert_Node_At_Position_Doubly(head_node, tail_node, D, 1)
+# display_doubly(head_node)
+# head_node, tail_node = insert_Node_At_Position_Doubly(head_node, tail_node, E, 2)
+# display_doubly(head_node)
